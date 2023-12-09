5f9ebac9cbaf6d5e40367b3728ec7993b905516d
Kahil Mohamed Amine <45295365+Solidesphere@users.noreply.github.com>
2023-12-09 13:46:57 +0100

    cartfunction && cart utils

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,29 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Form,
+} from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import Rating from "../components/Rating";
-import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
+import { addToCart } from "../slices/cartSlice";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [qty, setQty] = useState(1);
 
   const {
     data: product,
@@ -15,6 +31,11 @@ const ProductScreen = () => {
     isError,
     error,
   } = useGetProductDetailsQuery(productId);
+
+  const addTocartHandler = () => {
+    dispatch(addToCart({ ...product, qty }));
+    navigate("/cart");
+  };
 
   return (
     <>
@@ -70,11 +91,33 @@ const ProductScreen = () => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as="select"
+                          value={qty}
+                          name="qty"
+                          onChange={(e) => setQty(Number(e.target.value))}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
                 <ListGroup.Item>
                   <Button
                     className="btn-block w-100"
                     type="button"
                     disabled={product.countInStock === 0}
+                    onClick={addTocartHandler}
                   >
                     Add To Cart
                   </Button>
---
+++ frontend/utils/cartUtils.js
@@ -0,0 +1,25 @@
+export const addDecimlas = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
+export const updateCart = (state) => {
+  //calculate items price
+  state.itemsPrice = addDecimlas(
+    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  );
+
+  //calculate shipping price (if order is over 100 then 100$, else 10$ shipping)
+  state.shippingPrice = addDecimlas(state.itemsPrice > 100 ? 0 : 10);
+
+  //calculate tax price (15% tax)
+  state.taxPrice = addDecimlas(Number(0.15 * state.itemsPrice).toFixed(2));
+
+  //calculate total price
+  state.totalPrice = (
+    Number(state.itemsPrice) +
+    Number(state.shippingPrice) +
+    Number(state.taxPrice)
+  ).toFixed(2);
+  localStorage.setItem("cart", JSON.stringify(state));
+  return state;
+};
