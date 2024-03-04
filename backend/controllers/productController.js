2b8a55b8f78087e60dc82d61fdac1eae77ed041d
Kahil Mohamed Amine <45295365+Solidesphere@users.noreply.github.com>
2024-03-04 10:20:09 +0100

    adding product carousel

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -127,6 +127,14 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc Get top rated products
+//@route GET /api/products/top
+//@access Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+  res.status(200).json(products);
+});
+
 export {
   getProductById,
   getProducts,
@@ -134,4 +142,5 @@ export {
   updateProduct,
   deleteProduct,
   createProductReview,
+  getTopProducts,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,11 +6,13 @@ import {
   updateProduct,
   deleteProduct,
   createProductReview,
+  getTopProducts,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.get("/top", getTopProducts);
 router
   .route("/:id")
   .get(getProductById)
